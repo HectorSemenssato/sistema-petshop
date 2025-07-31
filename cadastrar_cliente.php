571ab8477814b52cc4c0f24a640bfb61b84815cb
HectorSemenssato <hector.semenssato@hotmail.com>
2025-07-30 22:11:52 -0300

    feat: telas de cadastro de animal e cliente (ainda em desenvolvimento)

--- cadastrar_cliente.php
+++ cadastrar_cliente.php
@@ -1,3 +1,6 @@
+<?php
+    include 'protege_pagina.php'; // Nosso guardião!
+?>
 <!doctype html>
 <html lang="en">
 
@@ -6,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de animal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles_cadastros.css">
+    <link rel="stylesheet" href="./css/styles_cadastros.css">
 </head>
 
 <body>
