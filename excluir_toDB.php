c1b90c9d73521b8b11f6190c6f4d429e9e5f7a90
HectorSemenssato <hector.semenssato@hotmail.com>
2025-07-31 23:38:51 -0300

    refactor: reestruturação para arquivo ser apenas código PHP para inserção do SweetAlert 2

--- excluir_toDB.php
+++ excluir_toDB.php
@@ -1,37 +1,22 @@
-<!doctype html>
-<html lang="en">
+<?php
+session_start();
+include 'conexao.php';
+include 'protege_pagina.php';
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Exclusão de agendamento</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+$idagendamento = $_GET['id'];
 
-</head>
+$sql = "DELETE from `agendamento` WHERE `id_agendamento` = ?";
 
-<body>
-  <div class="container">
-    <div class="row">
-      <?php
-      include "conexao.php";
-      $idagendamento = $_POST['id'];
+if ($stmt = $conn->prepare($sql)) {
+  $stmt->bind_param("i", $idagendamento);
 
-      $sql = "DELETE from `agendamento` WHERE `id_agendamento` = $idagendamento";
-
-      if (mysqli_query($conn, $sql)) {
-        mensagem("Agendamento excluído com sucesso :D", 'success');
-      } else {
-        mensagem("Não foi possível excluir este agendamento :(", 'danger');
-      }
-      ?>
-      <div class="d-flex justify-content-center gap-2">
-        <a href="index.php" class="btn btn-primary me-2">Ir para o início</a>
-        <a href="consultar.php" class="btn btn-success">Voltar para registro de agendamentos</a>
-      </div>
-    </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-</body>
-
-</html>
+  if ($stmt->execute() == true) {
+    $_SESSION['mensagem_sucesso'] = "Exclusão feita com sucesso!";
+  } else {
+    $_SESSION['mensagem_falha'] = "Não foi possível excluir este agendamento.";
+  }
+  $stmt->close();
+  $conn->close();
+  header('Location: consultar.php');
+  exit();
+}
