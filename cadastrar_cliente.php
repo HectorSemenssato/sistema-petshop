fa89e746520fe9a6db90ddf4cae3644d8ee70d9f
HectorSemenssato <hector.semenssato@hotmail.com>
2025-09-03 15:27:03 -0300

    style: mudança para novo css e correções de interface

--- cadastrar_cliente.php
+++ cadastrar_cliente.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de animal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/styles_cadastros.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 
 <body>
@@ -21,10 +21,6 @@
             <div class="col">
                 <form action="cadastrar_toDB.php">
                     <div class="input__container input_nomeanimal" data-label="nome do animal">
-                        <div class="shadow__input"></div>
-                        <button class="input__button__shadow">
-                            <p>🐾</p>
-                        </button>
                         <input type="text" name="nomeanimal" class="input__search search_cliente" placeholder="Digite o nome do bichinho" />
                     </div>
                     <div class="input__container input_idade" data-label="idade">
@@ -43,8 +39,8 @@
                         <div class="shadow__input"></div>
                         <input type="text" name="idadeanimal" class="input__search search_tutor" placeholder="Digite o nome do tutor" />
                     </div>
-                    <button type="submit" class="btn btn-success">Cadastrar animal</button>
-                    <a href="index.php" class="btn btn-info">Voltar para o início</a>
+                    <button type="submit" class="btn btn-confirmaformulario">Cadastrar animal</button>
+                    <a href="index.php" class="btn btn-inicio">Voltar para o início</a>
                 </form>
             </div>
         </div>
